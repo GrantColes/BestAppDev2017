84d23b4df84064548b52fe9c208529a64ee4d1fc
Coffee-is-Life <clay.patterson@okstate.edu>
2017-04-06 18:18:14 -0500

    Comments made to various files

---
+++ BuildConfig.java
@@ -0,0 +1,12 @@
+package com.example.HeartRate;
+
+public final class BuildConfig
+{
+  public static final boolean DEBUG = true;
+}
+
+
+/* Location:              C:\Users\Grant\Desktop\ConversionFolder\output2.jar!\com\example\HeartRate\BuildConfig.class
+ * Java compiler version: 6 (50.0)
+ * JD-Core Version:       0.7.1
+ */
